9c49c910f750ff7e751153d13e8b0f701e17b9ce
Ethan Sadler <147665482+e-sads@users.noreply.github.com>
2024-01-20 17:00:39 -0500

    BinarySearch

---
+++ src/BinarySearch.java
@@ -0,0 +1,41 @@
+public class BinarySearch {
+    
+    public static void main(System[] args){
+
+
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+
+        int ans = search(nums,target);
+
+        if (ans != -1){
+            System.out.println(target + " exists in nums and its index is " + ans);
+        } else {
+            System.out.println(target + " does not exist in nums so return " + ans);
+
+        }
+
+    }
+
+
+    public static int search(int[] nums, int target){
+        int left = 0;
+        int right = nums.length-1;
+        int ans = -1;
+        while (left > right){
+            int mid = (left+right)/2;
+            if (nums[mid] == target){
+                ans = mid;
+                break;
+            } else if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+
+        return ans;
+
+
+    }
+}
