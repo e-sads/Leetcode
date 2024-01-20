900401a753edd1079f12e1e017df70919f2c1052
Ethan Sadler <147665482+e-sads@users.noreply.github.com>
2024-01-20 17:02:10 -0500

    Update BinarySearch.java

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -18,24 +18,22 @@ public class BinarySearch {
     }
 
 
-    public static int search(int[] nums, int target){
+    public static int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length-1;
         int ans = -1;
-        while (left > right){
+        while (left <= right){
             int mid = (left+right)/2;
             if (nums[mid] == target){
                 ans = mid;
                 break;
             } else if (nums[mid] < target) {
-                left = mid;
+                left = mid+1;
             } else {
-                right = mid;
+                right = mid-1;
             }
         }
 
         return ans;
-
-
     }
 }
